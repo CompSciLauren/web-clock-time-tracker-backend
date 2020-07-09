87b1fad02da4e77a61e1a29f4627c74d21612bc7
Cameron Bahl <sportslvr5@gmail.com>
2020-07-09 16:23:24 -0500

    Making endpoints

--- src/routes/timecode.route.js
+++ src/routes/timecode.route.js
@@ -4,6 +4,13 @@ const router = express.Router();
 
 const TimeCodeController = require('../controllers/timecode.controller');
 
+
 router.get('/', TimeCodeController.getTimeCode);
+
+router.get('/api/timecodes', function(req, res) {
+  TimeCode.find({}).then(eachOne =>{
+    res.json(eachOne);
+   })
+})
 
 module.exports = router;
