e2e2a96e6799ab07b81266d50e4bc81f55158421
Cameron Bahl <sportslvr5@gmail.com>
2020-07-09 13:10:36 -0500

    Debugging Timecode

--- src/controllers/timecode.controller.js
+++ src/controllers/timecode.controller.js
@@ -1,6 +1,6 @@
 const TimeCodeService = require('../services/timecode.service');
 
-exports.getTimeCode = async function (req, res, next) {
+exports.getTimeCode = async function (req, res) {
   // Validate request parameters
   const page = req.params.page ? req.params.page : 1;
   const limit = req.params.limit ? req.params.limit : 10;
@@ -9,5 +9,5 @@ exports.getTimeCode = async function (req, res, next) {
     return res.status(200).json({ status: 200, data: timeCode, message: 'Succesfully generated time code' });
   } catch (e) {
     return res.status(400).json({ status: 400, message: e.message });
-    }
+  }
 };
