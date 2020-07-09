98994f1583f14e0f20cf7a9b81c11c7df287dd26
Cameron Bahl <sportslvr5@gmail.com>
2020-07-09 18:34:08 -0500

    Making endpoints

--- src/models/timecode.model.js
+++ src/models/timecode.model.js
@@ -5,10 +5,6 @@ const TimeCodeSchema = new Schema({
   description: String,
 });
 
-TimeCodeSchema.methods.getTimeCode = function () {
-  return Math.floor((this.endDate - this.startDate) / 1000);
-};
-
 const TimeCode = model('Timecode', TimeCodeSchema);
 
 export default TimeCode;
