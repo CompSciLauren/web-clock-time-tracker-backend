faeee0695fb8fc47cc1384fab0d0e889e5740938
Cameron Bahl <sportslvr5@gmail.com>
2020-07-09 12:37:55 -0500

    Debugging Timecode

--- src/controllers/timecode.controller.js
+++ src/controllers/timecode.controller.js
@@ -1,14 +1,13 @@
 const TimeCodeService = require('../services/timecode.service');
 
 exports.getTimeCode = async function (req, res, next) {
-    // Validate request parameters
+  // Validate request parameters
   const page = req.params.page ? req.params.page : 1;
   const limit = req.params.limit ? req.params.limit : 10;
   try {
-      var timeCode = await TimeCodeService.getTimeCode({}, page, limit);
-      return res.status(200).json({ status: 200, data: timeCode, message: 'Succesfully generated time code' });
-   } catch (e) {
-      return res.status(400).json({ status: 400, message: e.message });
+    const timeCode = await TimeCodeService.getTimeCode({}, page, limit);
+    return res.status(200).json({ status: 200, data: timeCode, message: 'Succesfully generated time code' });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
     }
-    next();
-}
+};
--- src/services/timecode.service.js
+++ src/services/timecode.service.js
@@ -2,9 +2,9 @@ const TimeCode = require('../models/timecode.model');
 
 exports.getTimeCode = async function (query, page, limit) {
   try {
-      const timeCode = await TimeCode.find(query);
-      return Date.now();
+    const timeCode = await TimeCode.find(query);
+    return Date.now();
   } catch (e) {
-      throw Error('Error while generating time code');
+    throw Error('Error while generating time code');
   }
-}
+};
