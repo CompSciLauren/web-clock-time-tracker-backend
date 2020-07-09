9d5126bfd48748a5a931d146a354450b75d41c4e
Cameron Bahl <sportslvr5@gmail.com>
2020-07-09 18:40:38 -0500

    Making endpoints

--- src/controllers/timecode.controller.js
+++ src/controllers/timecode.controller.js
@@ -1,10 +1,10 @@
 import TimeCode from '../models/timecode.model';
 
 const timeCodeController = async (req, res) => {
-  try{
-  TimeCode.register(new TimeCode({ id: req.body.id }), req.body.description);
-  res.status(200);
-  res.send('Ok');
+  try {
+    TimeCode.register(new TimeCode({ id: req.body.id }), req.body.description);
+    res.status(200);
+    res.send('Ok');
   } catch (e) {
     res.status(400);
     res.send(e.message);
