b1d5d89f7673c75757d029fce4fcb657ebb1d641
Eric McCullough <eric96@live.missouristate.edu>
2020-07-09 10:42:30 -0500

    Add mongo configs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 import addRoutes from './routes/routes.index';
+import dbConfig from './config/database';
+
+mongoose.connect(dbConfig.url);
 
 const app = express();
 
