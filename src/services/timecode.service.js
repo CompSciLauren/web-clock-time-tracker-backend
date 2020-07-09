d7850b45bf12591fc763106169ab711b66e2f071
Cameron Bahl <sportslvr5@gmail.com>
2020-07-09 12:32:14 -0500

    Debugging Timecode

--- src/services/timecode.service.js
+++ src/services/timecode.service.js
@@ -1,10 +1,10 @@
-var TimeCode = require('../models/timecode.model')
+const TimeCode = require('../models/timecode.model');
 
 exports.getTimeCode = async function (query, page, limit) {
-    try {
-        var timeCode = await TimeCode.find(query)
-        return Date.now();
-    } catch (e) {
-        throw Error('Error while generating time code')
-    }
+  try {
+      const timeCode = await TimeCode.find(query);
+      return Date.now();
+  } catch (e) {
+      throw Error('Error while generating time code');
+  }
 }
