b5d39d30d96ec11c9a6d3bb760535d0f8c384502
Cameron Bahl <sportslvr5@gmail.com>
2020-07-09 19:37:00 -0500

    Making endpoints

--- src/controllers/timecode.controller.js
+++ src/controllers/timecode.controller.js
@@ -2,7 +2,8 @@ import TimeCode from '../models/timecode.model';
 
 const timeCodeController = async (req, res) => {
   try {
-    TimeCode.createTimeCode(new TimeCode({ id: req.body.id }), req.body.description);
+    const timeCode = new TimeCode({ id: req.body.id }), req.body.description;
+    timeCode.save();
     res.status(200);
     res.send('Ok');
   } catch (e) {
--- src/models/timecode.model.js
+++ src/models/timecode.model.js
@@ -7,9 +7,4 @@ const TimeCodeSchema = new Schema({
 
 const TimeCode = model('Timecode', TimeCodeSchema);
 
-TimeCodeSchema.methods.createTimeCode = function (timeCodeData) {
-  const timeCode = new TimeCode(timeCodeData);
-  return timeCode.save();
-};
-
 export default TimeCode;
